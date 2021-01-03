e0a2b2c174f53ecb1e8a29fec6a5eff4cf3be639
Saral Tayal <thunderbird211@gmail.com>
2021-01-03 16:57:02 +0800

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -3,13 +3,22 @@
 # This script launches all the relavent ROS nodes in new tabls and helps the sourcing process.
 
 # Launch Lidar
-lxterminal --command 'cd ~/slam2 && source devel/setup.bash && roslaunch ydlidar lidar.launch'
-# Wait a couple of seconds
-sleep 3
+xterm -title "App 1" -e "source devel/setup.bash && roslaunch ydlidar lidar.launch" &
+# lxterminal --command 'cd ~/slam2 && source devel/setup.bash && roslaunch ydlidar lidar.launch'
+
+# Wait a couple of seconds for the lidar to fire up
+sleep 5
+
 # Launch SLAM
-lxterminal --command 'cd ~/slam2 && source devel/setup.bash && roslaunch slam slam.launch'
+xterm -title "App 2" -e "source devel/setup.bash && roslaunch slam slam.launch" &
+# lxterminal --command 'cd ~/slam2 && source devel/setup.bash && roslaunch slam slam.launch'
+
 # Launch Navigation (local + global costmaps & planning)
-lxterminal --command 'cd ~/slam2 && source devel/setup.bash && roslaunch rover2_2dnav move_base.launch'
+xterm -title "App 3" -e "source devel/setup.bash && roslaunch rover2_2dnav move_base.launch" &
+# lxterminal --command 'cd ~/slam2 && source devel/setup.bash && roslaunch rover2_2dnav move_base.launch'
+
 # Launch Robot Pose Generator 
-lxterminal --command 'cd ~/slam2 && source devel/setup.bash && rosrun robot_pose_publisher robot_pose_publisher'
+xterm -title "App 4" -e "source devel/setup.bash && rosrun robot_pose_publisher robot_pose_publisher" 
+# lxterminal --command 'cd ~/slam2 && source devel/setup.bash && rosrun robot_pose_publisher robot_pose_publisher'
+
 # Launch Motor controller and differential drive converter
